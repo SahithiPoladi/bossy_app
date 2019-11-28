c2fd8f023174ac9d9852e9e7813db8cc0f675e1a
SahithiPoladi <sahithi300.poladi@gmail.com>
2019-11-28 02:27:04 -0600

    Final Commit

--- server.js
+++ server.js
@@ -1,7 +1,23 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerUI = require('swagger-ui-express');
 
 const app = express();
+
+// Extended: https://swagger.io/specification/#infoObject
+const swaggerOptions = {
+    info: {
+        title: 'TODO API',
+        description: "Pointy Haired Boss wants the TODO application",
+        contact: {
+            name: "Sahithi"
+        },
+        server: ["http://localhost:3000"]
+    },
+
+    apis: [server.js]
+},
 
 // Connect Database
 connectDB();
@@ -13,6 +29,10 @@ app.get('/', (req, res) => res.send('API Running'));
 //Define Routes
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/task', require('./routes/api/task'));
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 const PORT = process.env.PORT || 3000;
 
